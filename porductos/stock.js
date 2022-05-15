d409807072f72071968e0d3105c1251e7bf56701
Emiliano <canelladaemiliano@gmail.com>
2022-05-15 17:26:25 -0300

    Sin agregar funcionalidad

---
+++ porductos/stock.js
@@ -0,0 +1,50 @@
+let stockProductos = [
+    {
+        'img': "/images/img-cafetera-moulinex.jpg",
+        'precios': 6770,
+        'descuentos': 40,
+        'descripcion-articulo': 'Cafetera Moulinex'
+    },
+    {
+        'img': "/images/img-macbook-pro-2019.jpg",
+        'precios': 230.000,
+        'descuentos': 20,
+        'descripcion-articulo': 'MacBook Pro 2019'
+    },
+    {
+        'img': "/images/img-samsung-galaxy-s10.jpg",
+        'precios': 70.500,
+        'descuentos': 10,
+        'descripcion-articulo': 'Samsung Galaxy S10'
+    },
+    {
+        'img': "/images/img-tv-samsung-smart.jpg",
+        'precios': 23.200,
+        'descuentos': 5,
+        'descripcion-articulo': 'SmartTV Samsung 43'
+    },
+    {
+        'img': "/images/img-cafetera-moulinex.jpg",
+        'precios': 6770,
+        'descuentos': 40,
+        'descripcion-articulo': 'Cafetera Moulinex'
+    },
+    {
+        'img': "/images/img-macbook-pro-2019.jpg",
+        'precios': 230.000,
+        'descuentos': 20,
+        'descripcion-articulo': 'MacBook Pro 2019'
+    },
+    {
+        'img': "/images/img-samsung-galaxy-s10.jpg",
+        'precios': 70.500,
+        'descuentos': 10,
+        'descripcion-articulo': 'Samsung Galaxy S10'
+    },
+    {
+        'img': "/images/img-tv-samsung-smart.jpg",
+        'precios': 23.200,
+        'descuentos': 5,
+        'descripcion-articulo': 'SmartTV Samsung 43'
+    }
+]
